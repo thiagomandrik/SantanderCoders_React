735855b537b4b835c22d69a6382ada85c73fc745
Thiago Mandrik <thiagomandrik@gmail.com>
2023-05-28 17:37:06 -0300

    efeitos colaterais

--- efeitos-colaterais/src/App.jsx
+++ efeitos-colaterais/src/App.jsx
@@ -1,24 +1,46 @@
-import { useState } from 'react'
+import { useEffect } from "react";
+import { useState } from "react";
 
 const minhaLista = [
-  {id:'1', value:'Frutas'},
-  {id:'2', value:'Legumes'},
-  {id:'3', value:'Carnes'},
-]
+  { id: "1", value: "Frutas" },
+  { id: "2", value: "Legumes" },
+  { id: "3", value: "Carnes" },
+];
 
 function App() {
+  const [produtos, setProdutos] = useState(minhaLista);
+  const [pesquisa, setPesquisa] = useState("");
+
+  useEffect(() => {
+    if(pesquisa){
+      const novaLista = minhaLista.filter((item) => {
+        return item.value.toLowerCase().includes(pesquisa.toLowerCase());
+      });
+      setProdutos(novaLista)
+    }else{
+      setProdutos(minhaLista)
+    }
+  }, [pesquisa]);
+
   return (
     <div>
       <h1>Efeitos colaterais</h1>
-      {minhaLista.map((item) => {
-        return(
+      <input
+        value={pesquisa}
+        onChange={(e) => setPesquisa(e.target.value)}
+        placeholder="Pesquise aqui"
+        type="text"
+      />
+
+      {produtos.map((item) => {
+        return (
           <div key={item.id}>
             <h4>{item.value}</h4>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
